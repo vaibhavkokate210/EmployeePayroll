7d87fb3afa9913c317780e52b06b79bcee2941b9
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-09-17 20:16:54 +0530

    [Vaibhav] added creating watch service to watch particular directory

--- src/test/java/com/bidgelabz/employeeparolltest/NIOFileApiTest.java
+++ src/test/java/com/bidgelabz/employeeparolltest/NIOFileApiTest.java
@@ -41,4 +41,12 @@ public class NIOFileApiTest
 		Files.newDirectoryStream(playPath).forEach(System.out::println);
 		Files.newDirectoryStream(playPath,path->path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
 	}
+	
+	@Test
+	public void givenDirectory_WhenWatched_ShouldReturnAllActivities() throws IOException
+	{
+		Path dir = Paths.get(HOME+'/'+PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
+	}
 }
